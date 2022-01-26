dd254c39e28fdc65dd3e966dc0d3a0eb246c6422
indraja <gindraja331@gmail.com>
2022-01-26 10:08:56 +0530

    cc removed
    Facebook class cc removed

--- src/launchBrowsers/FaceBook.java
+++ src/launchBrowsers/FaceBook.java
@@ -11,7 +11,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class FaceBook {
 static WebDriver driver;
 	public static void main(String[] args) throws InterruptedException {
-	cc
+	
 		driver.get("http://google.com/");
 		Thread.sleep(4000);
 		List<WebElement>links=driver.findElements(By.tagName("a"));
